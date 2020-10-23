31d4c2f3209893c300e6e557533417bfe8242cc3
Mathieu Robert <robert.mathieu.p@gmail.com>
2020-10-23 15:08:46 +0200

    exo 2

--- exercise-1/string.js
+++ exercise-1/string.js
@@ -54,3 +54,24 @@ function propAccess(obj, type) {
 }
 
 propAccess(prairie, "animal.type.name");
+
+
+/* Exos - partie 2 */
+
+function typeCheckV1(arg1, arg2){
+    let verifType = '';
+    if(typeof(arg1) == 'number' && arg2 == 'number') {
+        verifType = true;
+    }
+    else if(typeof(arg1) == 'string' && arg2 == 'string'){
+        verifType = true;
+    }
+    else if(typeof(arg1) == 'boolean' && arg2 == 'boolean') {
+        verifType = true;
+    }
+    else if(arg1 == '' && arg2 == 'null') {
+        verifType = true;
+    }
+    console.log(verifType);
+}
+typeCheckV1('','null');
