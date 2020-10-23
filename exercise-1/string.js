3adca96390a348bbf8d4d4d97028f4536e4f88c1
Mathieu Robert <robert.mathieu.p@gmail.com>
2020-10-23 15:51:27 +0200

    aaa

--- exercise-1/string.js
+++ exercise-1/string.js
@@ -1,56 +1,91 @@
 function ucfirst(chaine) {
     if (typeof chaine !== "string" || chaine === "") return "";
     return chaine.charAt(0).toUpperCase() + chaine.slice(1);
-}
-
-
-
-function capitalize(chaine) {
+  }
+  function capitalize(chaine) {
     if (typeof chaine !== "string" || chaine === "") return "";
-       let splitStr = chaine.toLowerCase().split(' ');
-   for (let i = 0; i < splitStr.length; i++) {
-       splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
-   }
-   return splitStr.join(' '); 
-}
-
-
-function camelCase(chaine) {
-    if (typeof chaine !== "string" || chaine === "") return "";
-    let splitStr = chaine.toLowerCase().split(' ');
-   for (let i = 0; i < splitStr.length; i++) {
-       splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
-   }
-   return splitStr.join(''); 
-
-}
-
-
-function snake_case(chaine) {
-    if (typeof chaine !== "string" || chaine === "") return "";
-    var splitStr = chaine.toLowerCase().split(' ');
-    return splitStr.join('_'); 
-}
-
-
-function leet(chaine) {
-    if (typeof chaine !== "string" || chaine === "") return "";
-    let crypt = chaine.replace(/[aA]/g, '4').replace(/[eE]/g, '3').replace(/[iI]/g, '1').replace(/[oO]/g, '0').replace(/[uU]/g, '(_)').replace(/[yY]/g, '7');
-    return crypt;
-}
-
-
-
-let prairie = { 
-    animal: {
-        type: {
-            name : 'chien'
-        }
+  
+    return chaine
+      .split(" ")
+      .map((word) => ucfirst(word.toLowerCase()))
+      .join(" ");
+  }
+  
+  console.log(capitalize("bonjour LA FraNce"));
+  
+  function camelCase(chaine) {
+    return capitalize(chaine).replace(/\W/g, "");
+  }
+  
+  console.log(camelCase("bonjour LA 'FraNce"));
+  
+  function snake_case(chaine) {
+    return chaine.toLowerCase().replace(/(\W)+/g, "-");
+  }
+  
+  console.log(snake_case("bonjour LA 'FraNce"));
+  
+  function leet(chaine) {
+    return chaine.replace(/[AEIOUY]/gi, function (e) {
+      switch (e.toLowerCase()) {
+        case "a":
+          return 4;
+        case "e":
+          return 3;
+        case "i":
+          return 1;
+        case "o":
+          return 0;
+        case "u":
+          return "(_)";
+        case "y":
+          return 7;
+      }
+    });
+  }
+  
+  console.log(leet("AnaCOnDA"));
+  
+  function verlan(chaine) {
+    return chaine
+      .split(" ")
+      .map((word) => {
+        return word.split("").reverse().join("");
+      })
+      .join(" ");
+  }
+  
+  console.log(verlan("bonjour la france"));
+  
+  function yoda(chaine) {
+    return chaine.split(" ").reverse().join(" ");
+  }
+  
+  console.log(yoda("bonjour la france"));
+  
+  function vig(string, code) {
+    if (typeof string !== "string") return "";
+    if (string.length === 0) return string;
+  
+    while (code.length < string.length) {
+      code += code;
     }
-}
-
-function propAccess(obj, type) {
-
-}
-
-propAccess(prairie, "animal.type.name");
+    code = code.substr(0, string.length);
+    let codeIndex = 0;
+  
+    return string
+      .split("")
+      .map((letter, index) => {
+        letter = letter.toLowerCase();
+        const aCode = "a".charCodeAt(0);
+        const letterNumber = letter.charCodeAt(0) - aCode; // [0-25]
+  
+        if (letterNumber < 0 || letterNumber > 25) return letter;
+  
+        const codeNumber = code.charCodeAt(codeIndex) - aCode; // [0-25]
+        codeIndex++;
+  
+        return String.fromCharCode(((letterNumber + codeNumber) % 26) + aCode);
+      })
+      .join("");
+  }
