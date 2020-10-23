33ddfde2083f89bf7dd056d349f285944118c819
Mathieu Robert <robert.mathieu.p@gmail.com>
2020-10-23 15:18:31 +0200

    aa

--- exercice-2/type-check.js
+++ exercice-2/type-check.js
@@ -2,18 +2,8 @@
 
 function type_check_v1(arg1, arg2){
     let verifType = '';
-    if(typeof(arg1) == 'number' && arg2 == 'number') {
-        verifType = true;
-    }
-    else if(typeof(arg1) == 'string' && arg2 == 'string'){
-        verifType = true;
-    }
-    else if(typeof(arg1) == 'boolean' && arg2 == 'boolean') {
-        verifType = true;
-    }
-    else if(arg1 == '' && arg2 == 'null') {
+    if(typeof(arg1) === arg2) {
         verifType = true;
     }
     console.log(verifType);
 }
-type_check_v1('','null');
