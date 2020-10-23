2f721cfc9f9e4ce32fdafce3950c6a2d25c956ce
Mathieu Robert <robert.mathieu.p@gmail.com>
2020-10-23 15:10:26 +0200

    exos 2

---
+++ exercice-2/string.js
@@ -0,0 +1,19 @@
+/* Exos - partie 2 */
+
+function typeCheckV1(arg1, arg2){
+    let verifType = '';
+    if(typeof(arg1) == 'number' && arg2 == 'number') {
+        verifType = true;
+    }
+    else if(typeof(arg1) == 'string' && arg2 == 'string'){
+        verifType = true;
+    }
+    else if(typeof(arg1) == 'boolean' && arg2 == 'boolean') {
+        verifType = true;
+    }
+    else if(arg1 == '' && arg2 == 'null') {
+        verifType = true;
+    }
+    console.log(verifType);
+}
+typeCheckV1('','null');
